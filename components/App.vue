f35ff0d9bcafce65ca50663e1bdb7cf83cd3a827
baiqiang.bq <baiqiang.bq@alibaba-inc.com>
2015-12-29 23:55:27 +0800

    vue

---
+++ components/App.vue
@@ -0,0 +1,15 @@
+<style scoped>
+    .container {
+        border: 1px solid #f00;
+    }
+
+    h2 {
+        color: #393;
+    }
+</style>
+
+<template>
+    <div class="container">
+        <h2>Hello from BaiQiang's Component!</h2>
+    </div>
+</template>
